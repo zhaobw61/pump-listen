d12a3d4ad6e4c679f96b9057939ecc5ed4cd83ac
bowen <boowenzhao641@gmail.com>
2024-11-14 18:55:03 +0800

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(routes);
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- mongooseIndex.js
+++ mongooseIndex.js
@@ -5,6 +5,9 @@ const otherString =
   'directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.0';
 const replicaSet = 'rs0';
 const port = 27017;
+console.log(
+  `mongodb://127.0.0.1:${port}/${dbName}?${otherString}&replicaSet=${replicaSet}`
+);
 mongoose
   .connect(
     `mongodb://127.0.0.1:${port}/${dbName}?${otherString}&replicaSet=${replicaSet}`,
