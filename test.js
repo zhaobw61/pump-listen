e2c5acb75fae4b0583633c860a103f03780868d8
bowen <boowenzhao641@gmail.com>
2024-11-21 21:35:53 +0800

    fix

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
-// import { sendMessage } from './services/discordServices.js';
+import { sendMessage } from './services/discordServices.js';
 
-// sendMessage({
-//   content: '这是一个测试消息',
-//   username: '警报机器人',
-// });
-
-console.log(process.env.NODE_ENV);
+for (let i = 0; i < 10; i++) {
+  try {
+    sendMessage({
+      content: '这是一个测试消息' + '---' + i,
+      username: '警报机器人',
+    });
+  } catch (error) {
+    console.log(i);
+  }
+}
