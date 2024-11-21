f6f75087dd9c80bd894efac71b5475bbc1fa2426
bowen <boowenzhao641@gmail.com>
2024-11-21 21:03:56 +0800

    fix

--- services/discordServices.js
+++ services/discordServices.js
@@ -4,7 +4,11 @@ const webhookUrl =
   'https://discord.com/api/webhooks/1308369183234461716/fiIn3NO0oa0NXJwTMPpsAKlSsRBhRfO3jwySRL8dovV4UMAMK10GbtQlZlYAv7nPCvQn'; // 替换为你的 Webhook URL
 
 export const sendMessage = async (content) => {
-  const res = axiosInstance.post(webhookUrl, content);
+  try {
+    const res = axiosInstance.post(webhookUrl, content);
+  } catch (error) {
+    console.log('send dis message error');
+  }
 };
 
 // const data = {
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-import { sendMessage } from './services/discordServices.js';
+// import { sendMessage } from './services/discordServices.js';
 
-sendMessage({
-  content: '这是一个测试消息',
-  username: '警报机器人',
-});
+// sendMessage({
+//   content: '这是一个测试消息',
+//   username: '警报机器人',
+// });
+
+console.log(process.env.NODE_ENV);
