04f5e37336b16da0ae374dbf7d31616e236c7260
bowen <boowenzhao641@gmail.com>
2024-11-22 11:58:50 +0800

    fix

--- test.js
+++ test.js
@@ -1,12 +1,1 @@
-import { sendMessage } from './services/discordServices.js';
-
-for (let i = 0; i < 10; i++) {
-  try {
-    sendMessage({
-      content: '这是一个测试消息' + '---' + i,
-      username: '警报机器人',
-    });
-  } catch (error) {
-    console.log(i);
-  }
-}
+import axios from 'axios';
