2067643746d29a3b9e643e711ce097e7eb97d554
bowen <boowenzhao641@gmail.com>
2024-11-27 21:40:44 +0800

    fix

--- services/twitterApiServices.js
+++ services/twitterApiServices.js
@@ -1,5 +1,6 @@
 import axiosInstance from './request.js';
 
+// 查询推特的搜索
 export const getLastSearchServices = async (content) => {
   try {
     const response = await axiosInstance.get(
@@ -14,5 +15,18 @@ export const getLastSearchServices = async (content) => {
     return data;
   } catch (error) {
     return {};
+  }
+};
+
+// 查询API剩余的次数
+export const getApiCount = async () => {
+  try {
+    const response = await axiosInstance.get(
+      `https://api.apidance.pro/key/gx8oca02jom03zrtf0ef5ign86ahdi`
+    );
+    const data = response.data;
+    return data;
+  } catch (error) {
+    return false;
   }
 };
