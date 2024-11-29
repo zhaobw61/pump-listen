b695f59c0cbcc2cb1a1a7fc7257b40e44f0cd957
bowen <boowenzhao641@gmail.com>
2024-11-29 17:03:46 +0800

    fix

--- controllers/progressCoinController.js
+++ controllers/progressCoinController.js
@@ -1,6 +1,10 @@
 import '../mongooseIndex.js';
 import { getProgressCoinListService } from '../services/gmgnServices.js';
-import { addProgressCoinService } from '../services/progressCoinServices.js';
+import {
+  addProgressCoinService,
+  delProgressCoinService,
+} from '../services/progressCoinServices.js';
+import progressCoin from '../models/progressCoinModels.js';
 
 async function getProgressCoinAddressList() {
   const res = await getProgressCoinListService();
@@ -19,12 +23,32 @@ async function getProgressCoinAddressList() {
   }
 }
 
+const deleteOverduProgressCoin = async () => {
+  const coinlist = await progressCoin.find().sort({ _id: 1 });
+  for (let i = 0; i < coinlist.length; i++) {
+    // 获取当前时间的时间戳
+    const now = Date.now(); // 当前时间（以毫秒为单位）
+
+    // 假设有一个时间，比如过去的某个时间
+    const pastTime = coinlist[i].creatTime; // 示例：过去时间的时间戳
+
+    // 判断时间是否超过 8 分钟
+    const eightMinutesInMs = 8 * 60 * 1000; // 8 分钟转换为毫秒
+    if (now - pastTime > eightMinutesInMs) {
+      delProgressCoinService({
+        address: coinlist[i].address,
+      });
+    }
+  }
+};
+
 let clearProgressCoinInter;
-let listenPumpTime = 1000 * 10; // 5分钟
+let listenPumpTime = 1000 * 10;
 export const startProgressCoinListen = () => {
   if (clearProgressCoinInter) clearInterval(clearProgressCoinInter);
   clearProgressCoinInter = setInterval(() => {
     getProgressCoinAddressList();
+    deleteOverduProgressCoin();
   }, listenPumpTime);
 };
 
