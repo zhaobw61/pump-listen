d693471b3965550525a7e93b248c70fbf4ac0f32
bowen <boowenzhao641@gmail.com>
2024-12-02 22:46:31 +0800

    fux

--- timeCheck.js
+++ timeCheck.js
@@ -1,4 +1,4 @@
 import moment from 'moment';
-console.log(new Date(1733037422330));
-const dateTime = moment(new Date(1733037422330)).format('YYYY-MM-DD HH:mm:ss');
+console.log(new Date());
+const dateTime = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
 console.log(dateTime);
