d13d0ef88091468d949868381f7e1aa6eec29eb1
bowen <boowenzhao641@gmail.com>
2024-12-02 21:48:32 +0800

    fix

--- services/twitterApiServices.js
+++ services/twitterApiServices.js
@@ -4,15 +4,30 @@ import axiosInstance from './request.js';
 export const getLastSearchServices = async (content) => {
   try {
     const response = await axiosInstance.get(
-      `https://api.apidance.pro/sapi/Search?q=${content}&cursor&sort_by=Latest`,
-      {
-        headers: {
-          apikey: 'gx8oca02jom03zrtf0ef5ign86ahdi',
-        },
-      }
+      `https://twitter.good6.top/api/base/apitools/search?any=${content}&apiKey=1547220975078735873OGMweEFHRUFIUzJNEkycG54ZW54SEp&product=Latest`
     );
-    const data = response.data;
-    return data;
+    const oriList =
+      response.data.data.data.search_by_raw_query.search_timeline.timeline
+        .instructions[0].entries;
+    let list = { tweets: [] };
+    if (oriList && oriList.length) {
+      oriList.forEach((item) => {
+        const entryType = item.content.entryType;
+        if (entryType == 'TimelineTimelineItem') {
+          const legacy = item.content.itemContent.tweet_results.result.legacy;
+          const user_results =
+            item.content.itemContent.tweet_results.result.core.user_results;
+          list.tweets.push({
+            tweet_id: legacy.id_str,
+            user_id: user_results.result.rest_id,
+            text: legacy.full_text,
+            created_at: legacy.created_at,
+            screen_name: user_results.result.legacy.screen_name,
+          });
+        }
+      });
+    }
+    return list;
   } catch (error) {
     console.log('推特搜索失败');
     return {};
--- test.js
+++ test.js
@@ -2,21 +2,26 @@ import axios from 'axios';
 import fs from 'fs';
 axios({
   method: 'get',
-  url: 'https://api.apidance.pro/sapi/Search?q=7RVwnhX56GaYQcsxisJ7wCaMMnJam7TNjYiNm9tGpump&cursor&sort_by=Latest',
-  headers: {
-    apikey: 'gx8oca02jom03zrtf0ef5ign86ahdi',
-  },
-  // redirect: 'follow',
+  url: 'https://twitter.good6.top/api/base/apitools/search?any=ArZxZDZGPF3uq6rjPtduqi2HSujzGy43reM922depump&apiKey=1547220975078735873OGMweEFHRUFIUzJNEkycG54ZW54SEp&product=Latest',
 })
   .then(function (response) {
-    fs.writeFile('./zzz.js', JSON.stringify(response.data), (err) => {
+    fs.writeFile('./xxx.json', response.data.data, (err) => {
       if (err) {
         console.error(err);
       }
       // file written successfully
     });
-    console.log(JSON.stringify(response.data));
   })
   .catch(function (error) {
     console.log(error);
   });
+
+// const options = { method: 'GET', headers: { accept: '*/*' } };
+
+// fetch(
+//   'https://twitter.good6.top/api/base/apitools/search?any=ArZxZDZGPF3uq6rjPtduqi2HSujzGy43reM922depump&apiKey=1547220975078735873OGMweEFHRUFIUzJNEkycG54ZW54SEp',
+//   options
+// )
+//   .then((res) => res.json())
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
