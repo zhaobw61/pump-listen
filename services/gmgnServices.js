5f026326e81d667ea0868de4968ca6c909133fd8
bowen <boowenzhao641@gmail.com>
2025-02-13 23:33:23 +0800

    fix: 修改请求路径

--- services/gmgnServices.js
+++ services/gmgnServices.js
@@ -18,7 +18,7 @@ export const getHotCoinListService = async () => {
 export const getProgressCoinListService = async () => {
   try {
     const response = await axiosInstance.get(
-      `https://gmgn.ai/defi/quotation/v1/rank/sol/pump/1h?limit=3&orderby=progress&direction=desc&pump=true`
+      `https://gmgn.ai/defi/quotation/v1/rank/sol/pump/1h?device_id=903d9bb5-6e81-441f-8e84-cdd57c5ed7dd&client_id=gmgn_web_2025.0212.174439&from_app=gmgn&app_ver=2025.0212.174439&tz_name=Asia%2FShanghai&tz_offset=28800&app_lang=en&limit=100&orderby=progress&direction=desc&pump=true&max_created=1h`
     );
     const data = response.data;
     return data;
