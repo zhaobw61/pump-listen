4521c01157da326f83b29709d7df8c4f88186f04
bowen <boowenzhao641@gmail.com>
2025-02-13 23:27:15 +0800

    fix: add log

--- services/gmgnServices.js
+++ services/gmgnServices.js
@@ -2,11 +2,13 @@ import axiosInstance from './request.js';
 
 // 获取热门币种
 export const getHotCoinListService = async () => {
+  console.log('getHotCoinListService');
   try {
     const response = await axiosInstance.get(
       `https://gmgn.ai/defi/quotation/v1/rank/sol/pump/1h?device_id=903d9bb5-6e81-441f-8e84-cdd57c5ed7dd&client_id=gmgn_web_2025.0212.174439&from_app=gmgn&app_ver=2025.0212.174439&tz_name=Asia%2FShanghai&tz_offset=28800&app_lang=en&limit=100&orderby=progress&direction=desc&pump=true&max_created=1h`
     );
     const data = response.data;
+    console.log(data);
     return data;
   } catch (error) {
     console.log('获取热门币种失败  getHotCoinListService');
