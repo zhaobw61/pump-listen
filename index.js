8a8402acc612d3a40af2b3e6290405372ef469c3
bowen <boowenzhao641@gmail.com>
2025-02-13 23:31:04 +0800

    fix: 修改配置

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import routes from './routes/index.js';
 import './mongooseIndex.js';
 
-import { startGmgnListen } from './controllers/gmgnController.js';
-// import { startProgressCoinListen } from './controllers/progressCoinController.js';
+// import { startGmgnListen } from './controllers/gmgnController.js';
+import { startProgressCoinListen } from './controllers/progressCoinController.js';
 // import { startListenTwitterLog } from './controllers/twitterLogController.js';
 
 const app = express();
@@ -16,6 +16,6 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-startGmgnListen(); // 监听热门币种
-// startProgressCoinListen(); // 监听即将打满币种
+// startGmgnListen(); // 监听热门币种
+startProgressCoinListen(); // 监听即将打满币种
 // startListenTwitterLog(); // 监听推特
--- services/gmgnServices.js
+++ services/gmgnServices.js
@@ -2,16 +2,14 @@ import axiosInstance from './request.js';
 
 // 获取热门币种
 export const getHotCoinListService = async () => {
-  console.log('getHotCoinListService');
   try {
     const response = await axiosInstance.get(
-      `https://gmgn.ai/defi/quotation/v1/rank/sol/pump/1h?device_id=903d9bb5-6e81-441f-8e84-cdd57c5ed7dd&client_id=gmgn_web_2025.0212.174439&from_app=gmgn&app_ver=2025.0212.174439&tz_name=Asia%2FShanghai&tz_offset=28800&app_lang=en&limit=100&orderby=progress&direction=desc&pump=true&max_created=1h`
+      `https://gmgn.ai/defi/quotation/v1/rank/sol/swaps/5m?orderby=change5m&direction=desc&filters[]=renounced&filters[]=frozen&filters[]=burn&filters[]=distribed&min_insider_rate=0&max_insider_rate=0.05&max_created=3h`
     );
     const data = response.data;
     console.log(data);
     return data;
   } catch (error) {
-    console.log('获取热门币种失败  getHotCoinListService');
     return false;
   }
 };
